e4e3b9414388274a9bf1c49f3a6ea29c1c35eebd
b0urbonn <163072843+b0urbonn@users.noreply.github.com>
2024-05-09 22:33:44 +0800

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -7,6 +7,38 @@ const productData = [
     price: 100,
     date: '2019-04-11',
   },
+  {
+    id: 2,
+    Product: 'Fries',
+    Category: 'Food',
+    Quantity: 5,
+    price: 100,
+    date: '2019-04-11',
+  },
+  {
+    id: 3,
+    Product: 'Etlog',
+    Category: 'Food',
+    Quantity: 2,
+    price: 100,
+    date: '2019-04-11',
+  },
+  {
+    id: 4,
+    Product: 'GoldenRice',
+    Category: 'Food',
+    Quantity: 2,
+    price: 100,
+    date: '2019-04-11',
+  },
+  {
+    id: 5,
+    Product: 'Asin',
+    Category: 'Food',
+    Quantity: 200,
+    price: 100,
+    date: '2019-04-11',
+  },
 ];
 
 export { productData };
